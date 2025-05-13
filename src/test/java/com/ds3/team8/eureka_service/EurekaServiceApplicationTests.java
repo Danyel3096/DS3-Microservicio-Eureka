c33e0cba24176bf993ca9deb4688505f12af44dc
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-05-12 22:18:43 -0500

    Estructura del microservicio

---
+++ src/test/java/com/ds3/team8/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
